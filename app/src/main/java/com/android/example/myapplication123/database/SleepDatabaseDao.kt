1b78dbdff7978d21bdd50c0e1943a6ddb76c5124
harshkumarkhatri <mailharshkhatri@gmail.com>
2020-05-29 14:02:57 +0530

    Completed the test for making databse

--- app/src/main/java/com/android/example/myapplication123/database/SleepDatabaseDao.kt
+++ app/src/main/java/com/android/example/myapplication123/database/SleepDatabaseDao.kt
@@ -24,32 +24,32 @@ interface SleepDatabaseDao{
     @Query("SELECT * FROM daily_sleep_quality_table ORDER BY nightId DESC")
     fun getAllNights(): LiveData<List<SleepNight>>
 }
-@Database(entities = [SleepNight::class], version = 1, exportSchema = false)
-abstract class SleepDatabase : RoomDatabase() {
-
-    abstract val sleepDatabaseDao: SleepDatabaseDao
-
-    companion object {
-
-        @Volatile
-        private var INSTANCE: SleepDatabase? = null
-
-        fun getInstance(context: Context): SleepDatabase {
-            synchronized(this) {
-                var instance = INSTANCE
-
-                if (instance == null) {
-                    instance = Room.databaseBuilder(
-                        context.applicationContext,
-                        SleepDatabase::class.java,
-                        "sleep_history_database"
-                    )
-                        .fallbackToDestructiveMigration()
-                        .build()
-                    INSTANCE = instance
-                }
-                return instance
-            }
-        }
-    }
-}
+//@Database(entities = [SleepNight::class], version = 1, exportSchema = false)
+//abstract class SleepDatabase : RoomDatabase() {
+//
+//    abstract val sleepDatabaseDao: SleepDatabaseDao
+//
+//    companion object {
+//
+//        @Volatile
+//        private var INSTANCE: SleepDatabase? = null
+//
+//        fun getInstance(context: Context): SleepDatabase {
+//            synchronized(this) {
+//                var instance = INSTANCE
+//
+//                if (instance == null) {
+//                    instance = Room.databaseBuilder(
+//                        context.applicationContext,
+//                        SleepDatabase::class.java,
+//                        "sleep_history_database"
+//                    )
+//                        .fallbackToDestructiveMigration()
+//                        .build()
+//                    INSTANCE = instance
+//                }
+//                return instance
+//            }
+//        }
+//    }
+//}

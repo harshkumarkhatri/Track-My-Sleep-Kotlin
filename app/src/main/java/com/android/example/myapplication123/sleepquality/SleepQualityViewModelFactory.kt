bc9624b8ff2ee9de0522900ebd5ee43aed08a1dc
harshkumarkhatri <mailharshkhatri@gmail.com>
2020-05-30 18:57:52 +0530

    Completed the functioning of buttons

--- app/src/main/java/com/android/example/myapplication123/sleepquality/SleepQualityViewModelFactory.kt
+++ app/src/main/java/com/android/example/myapplication123/sleepquality/SleepQualityViewModelFactory.kt
@@ -1,2 +1,19 @@
 package com.android.example.myapplication123.sleepquality
 
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.android.example.myapplication123.database.SleepDatabaseDao
+//import com.android.example.myapplication123.sleeptracker.SleepQualityViewModel
+
+class SleepQualityViewModelFactory(
+    private val sleepNightKey: Long,
+    private val dataSource: SleepDatabaseDao
+) : ViewModelProvider.Factory {
+    @Suppress("unchecked_cast")
+    override fun <T : ViewModel?> create(modelClass: Class<T>): T {
+        if (modelClass.isAssignableFrom(SleepQualityViewModel::class.java)) {
+            return SleepQualityViewModel(sleepNightKey, dataSource) as T
+        }
+        throw IllegalArgumentException("Unknown ViewModel class")
+    }
+}

584df11973e887bf477555ba1bd32f7786d8ba2b
harshkumarkhatri <mailharshkhatri@gmail.com>
2020-06-01 19:05:55 +0530

    Removed traditional viewer with a recycle viewer

--- app/src/main/java/com/android/example/myapplication123/sleeptracker/SleepTrackerFragment.kt
+++ app/src/main/java/com/android/example/myapplication123/sleeptracker/SleepTrackerFragment.kt
@@ -89,6 +89,13 @@ class SleepTrackerFragment : Fragment() {
                 sleepTrackerViewModel.doneNavigating()
             }
         })
+        val adapter = SleepNightAdapter()
+        binding.sleepList.adapter = adapter
+        sleepTrackerViewModel.nights.observe(viewLifecycleOwner, Observer {
+            it?.let {
+                adapter.data = it
+            }
+        })
         return binding.root
     }
 }
